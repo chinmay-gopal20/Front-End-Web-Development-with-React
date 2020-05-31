9ec8a2442300cbf9039790caac1695b87a7fe117
Chinmay G <gokulchinmay@gmail.com>
2020-05-31 13:05:27 +0530

    Configuring react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; //import bootstrap css first then our index.css to prevent index.css override bootstrap css
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
